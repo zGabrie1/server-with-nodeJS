298296aa1e9980bf1cd0196545b48896ef1ad660
zGabrie1 <gabriel980500@gmail.com>
2020-08-12 16:26:58 -0300

    sqliteKnex

---
+++ src/database/connction.ts
@@ -0,0 +1,10 @@
+import Knex from 'knex';
+
+import path from 'path';
+
+const db = Knex({
+    client: 'sqlite3',
+    connection: {
+        filename: path.resolve(__dirname, 'database.sqlite')
+    }
+})
