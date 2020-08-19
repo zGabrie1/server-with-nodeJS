24adf1d4f88b1a7d0652fa5d7564ab47134be8b1
zGabrie1 <gabriel980500@gmail.com>
2020-08-19 16:53:51 -0300

    errorReturn

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import db from './database/connction';
 import convertHoursToMinutes from './utils/convertHoursToMinutes';
 
@@ -22,35 +22,48 @@ routes.post('/classes', async (request, reponse) => {
         schedule
     } = request.body;
 
-    const insertedUsersIds = await db('users').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio,
-    });
+    const trx = await db.transaction();
 
-    const user_id = insertedUsersIds[0];
+    try {
+        const insertedUsersIds = await trx('users').insert({
+            name,
+            avatar,
+            whatsapp,
+            bio,
+        });
+    
+        const user_id = insertedUsersIds[0];
+    
+        const insertedClassesIds = await trx('classes').insert({
+            subject,
+            cost,
+            user_id,
+        });
+    
+        const class_id = insertedClassesIds[0];
+    
+        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+            return {
+                class_id,
+                week_day: scheduleItem.week_day,
+                from: convertHoursToMinutes(scheduleItem.from),
+                to: convertHoursToMinutes(scheduleItem.to)
+            };
+        })
+    
+        await trx('class_schedule').insert(classSchedule);
+    
+        await trx.commit();
+    
+        return reponse.status(201).send();
+    } catch (err) {
+        await trx.rollback();
 
-    const insertedClassesIds = await db('classes').insert({
-        subject,
-        cost,
-        user_id,
-    });
+        return response.status(400).json({
+            error:'Unexpected error while creating new class'
+        })
+    }
 
-    const class_id = insertedClassesIds[0];
-
-    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-        return {
-            class_id,
-            week_day: scheduleItem.week_day,
-            from: convertHoursToMinutes(scheduleItem.from),
-            to: convertHoursToMinutes(scheduleItem.to)
-        };
-    })
-
-    await db('class_schedule').insert(classSchedule);
-
-    return reponse.send();
 });
 
 export default routes;
